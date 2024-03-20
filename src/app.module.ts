5ae84dd8ab9cf99035951c9641737a7d0ab3ec86
John A. Quintero <quinteroberrio@gmail.com>
2024-03-19 22:38:50 -0500

    connection mongo successfuly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GatewayModule } from './websocket/websocket.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [GatewayModule],
+  imports: [GatewayModule,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: `mongodb://jaquinterob:matrimonio@mongo.jaquinterob.com:27777/qpocker?authSource=admin`,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })

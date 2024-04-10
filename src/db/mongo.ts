bc1c936f303a30f308be6546ed5e7bf9c7c2e4cf
John A. Quintero <jquintero@migrante.com>
2024-04-10 16:47:23 -0500

    lint fix

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,1 +1,2 @@
-export const MONGO_CONNECT_LOCAL = 'mongodb://test:test@localhost:27017?authSource=admin'
+export const MONGO_CONNECT_LOCAL =
+  'mongodb://test:test@localhost:27017?authSource=admin';
